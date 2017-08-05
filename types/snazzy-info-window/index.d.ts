bd8ae90c807ca4ef333c1138234f61a28fc843d6
milosd92 <milosdanilov@gmail.com>
2017-08-05 18:06:25 +0200

    added package version

--- types/snazzy-info-window/index.d.ts
+++ types/snazzy-info-window/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Snazzy Info Window
+// Type definitions for snazzy-info-window 1.1
 // Project: https://github.com/atmist/snazzy-info-window
 // Definitions by: Milos Danilov <https://github.com/milosd92/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
