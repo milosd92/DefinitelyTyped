a70c4815d0bdfd44cf44c7a33abb89fac10b61e8
milosd92 <milosdanilov@gmail.com>
2017-08-05 17:06:18 +0200

    added dynamic content test

--- types/snazzy-info-window/snazzy-info-window-tests.ts
+++ types/snazzy-info-window/snazzy-info-window-tests.ts
@@ -1,5 +1,6 @@
 import SnazzyInfoWindow = require('snazzy-info-window');
 import jQuery = require('jquery');
+import * as Handlebars from 'handlebars';
 
 const simple = () => {
     let myMap = new google.maps.Map($('.map-canvas')[0], {
@@ -57,6 +58,49 @@ const multipleMarkers = () => {
         });
         info.open();
     });
+};
+
+// Dynamic Content
+
+const dynamicContent = () => {
+    let myMap = new google.maps.Map($('.map-canvas')[0], {
+        zoom: 14,
+        center: new google.maps.LatLng(40.72, -74)
+    });
+    let myMarker = new google.maps.Marker({
+        map: myMap,
+        position: new google.maps.LatLng(40.72, -74),
+        draggable: true
+    });
+
+    Handlebars.registerHelper('formatDate', (date: Date) => {
+        return date && date.toLocaleTimeString();
+    });
+
+    let template = Handlebars.compile($('#marker-content-template').html());
+
+    let interval: number = 0;
+    let info = new SnazzyInfoWindow({
+        marker: myMarker,
+        callbacks: {
+            beforeOpen() {
+                info.setContent('loading...');
+            },
+            afterOpen() {
+                interval = setInterval(() => {
+                    info.setContent(template({
+                        date: new Date()
+                    }));
+                }, 1000);
+            },
+            afterClose() {
+                if (interval) {
+                    clearInterval(interval);
+                }
+            }
+        }
+    });
+    info.open();
 };
 
 // let infoWindow = new SnazzyInfoWindow({
